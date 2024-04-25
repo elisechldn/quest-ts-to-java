99aa9fa729021a85f3824ac0fc11cc97e05480ab
elisechldn <elise.chaldjian@gmail.com>
2024-04-25 18:49:16 +0200

    Création de 6 fichiers : 3 pour la conversion en .java et 3 en .class

---
+++ User.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class User {
+    private Date birthdate;
+    private int Size;
+    private Address address;
+    private ArrayList photo;
+}
